5ee0aa06bfce5f0773561768230b834410805c80
fasilofficial <muhammedfasilofficial@gmail.com>
2025-04-03 15:58:33 +0530

    Initial commit

---
+++ agents-xml.php
@@ -0,0 +1,25 @@
+<?php
+require 'utils/index.php';
+require_once __DIR__ . "/crest/settings.php";
+
+header('Content-Type: application/xml; charset=UTF-8');
+
+$baseUrl = WEB_HOOK_URL;
+$entityTypeId = AGENTS_ENTITY_TYPE_ID;
+$fields = [
+    'id',
+    'ufCrm8AgentName',
+    'ufCrm8AgentEmail',
+    'ufCrm8AgentMobile',
+    'ufCrm8AgentLicense',
+    'ufCrm8AgentPhoto'
+];
+
+$agents = fetchAllAgents($baseUrl, $entityTypeId, $fields);
+
+if (count($agents) > 0) {
+    $xml = generateAgentsXml($agents);
+    echo $xml;
+} else {
+    echo '<?xml version="1.0" encoding="UTF-8"?><agents></agents>';
+}
---
+++ crest/settings.php
@@ -0,0 +1,12 @@
+<?php
+define('WEB_HOOK_URL', 'https://exxcellenceambassadorsrealestate.bitrix24.com/rest/8/oon3qobf762aodpr/');
+define('C_REST_CLIENT_ID', 'local.67d0341c407f74.25967800');
+define('C_REST_CLIENT_SECRET', '8ubLbmDEmlHaZhnXMwZ39Sd5CTvAquSG6xeo2s0VIpiABtnOV9');
+
+define('DEVELOPERS_ENTITY_TYPE_ID', 1052);
+define('AGENTS_ENTITY_TYPE_ID', 1040);
+define('PF_LOCATIONS_ENTITY_TYPE_ID', 1044);
+define('BAYUT_LOCATIONS_ENTITY_TYPE_ID', 1048);
+define('LISTINGS_ENTITY_TYPE_ID', 1036);
+
+define('C_REST_BLOCK_LOG', true);
