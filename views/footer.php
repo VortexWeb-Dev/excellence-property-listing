d5ebddf98dd1a9c8639934c37a7ed5f3414625a2
fasilofficial <muhammedfasilofficial@gmail.com>
2025-04-09 14:32:35 +0530

    Update watermark image and function

--- views/footer.php
+++ views/footer.php
@@ -479,26 +479,13 @@
                 canvas.width = width;
                 canvas.height = height;
 
+                // Draw original image
                 ctx.drawImage(imageElement, 0, 0, width, height);
 
-                const watermarkAspect = watermarkImage.width / watermarkImage.height;
-                const imageAspect = width / height;
+                // Draw watermark image over it without resizing or positioning
+                ctx.drawImage(watermarkImage, 0, 0, width, height);
 
-                let watermarkWidth, watermarkHeight;
-
-                if (watermarkAspect > imageAspect) {
-                    watermarkWidth = width * 0.6;
-                    watermarkHeight = watermarkWidth / watermarkAspect;
-                } else {
-                    watermarkHeight = height * 0.6;
-                    watermarkWidth = watermarkHeight * watermarkAspect;
-                }
-
-                const xPosition = (width - watermarkWidth) / 2;
-                const yPosition = (height - watermarkHeight) / 2;
-
-                ctx.drawImage(watermarkImage, xPosition, yPosition, watermarkWidth, watermarkHeight);
-                const watermarkedImage = canvas.toDataURL('image/jpeg', 0.8);
+                const watermarkedImage = canvas.toDataURL('image/jpeg', 0.9);
                 resolve(watermarkedImage);
             };
 
@@ -507,6 +494,7 @@
             };
         });
     }
+
 
     // Function to add watermark text to the image
     function addWatermarkText(imageElement, watermarkText) {
